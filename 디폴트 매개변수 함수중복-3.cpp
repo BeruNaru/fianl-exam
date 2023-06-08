c3070137c78691538ec8eddd2def89da83f8aa7c
BeruNaru <91617802+BeruNaru@users.noreply.github.com>
2023-06-08 19:32:43 +0900

    Create 디폴트 매개변수 함수중복-3.cpp

---
+++ 디폴트 매개변수 함수중복-3.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+//함수 구현
+void f(char c = ' ', int line = 1) {
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < 10; j++)
+			cout << c;
+		cout << endl;
+	}
+
+}
+
+int main() {
+
+	f();
+	f('%');
+	f('@', 5);
+}
