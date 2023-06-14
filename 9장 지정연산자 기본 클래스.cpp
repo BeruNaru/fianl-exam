72aee9564582ff7992f1caaae207b5752c16b28e
BeruNaru <91617802+BeruNaru@users.noreply.github.com>
2023-06-14 12:19:41 +0900

    Create 9장 지정연산자 기본 클래스.cpp

---
+++ 9장 지정연산자 기본 클래스.cpp
@@ -0,0 +1,24 @@
+
+
+class Shape {
+public:
+	virtual void draw() { 
+		cout << "--Shape--";
+	}
+};
+
+class Circle : public Shape {
+public:
+	virtual void draw() { 
+		Shape::draw(); // 기본 클래스의 draw() 호출
+		cout << "Circle" << endl;
+	}
+};
+
+int main() {
+	Circle circle;
+	Shape * pShape = &circle;
+
+	pShape->draw(); 
+	pShape->Shape::draw(); 
+}
